6c929f58c2e41cbaa56f77f70034e33cf39ea622
outsidett <outside@pathianlabs.com>
2022-12-17 21:18:23 +0000

    connect to deployed version of the backend

--- covid-19/src/components/AllCountries/AllCountries.js
+++ covid-19/src/components/AllCountries/AllCountries.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Home/home.css";
 import Swal from 'sweetalert2';
+import {API} from '../../utilize/utilize';
 
 import oops from "../../assets/oops.png";
 import cookie from 'react-cookies'
 export default function AllCountries() {
-  const api ='http://localhost:3003'
   const [data, setData] = useState([]);
   const [loader, setLoader] = useState(true);
   const fetchAllData = async () => {
@@ -41,7 +41,7 @@ export default function AllCountries() {
         Date: e.Date,
       }
   
-       await axios.post(`${api}/addRecord`,newData ,{
+       await axios.post(`${API}/addRecord`,newData ,{
         headers: {
           Authorization: `Bearer ${cookie.load("token")}`,
         },
