6c65b0b984041fe09baf569a718d7b9567753fe3
outsidett <outside@pathianlabs.com>
2022-12-18 19:19:28 +0000

    fix button

--- covid-19/src/components/Home/home.js
+++ covid-19/src/components/Home/home.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [statistics, setStatistics] = useState({});
   const [cards, setCards] = useState([]);
   const [url, SetUrl] = useState(
-    "https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"
+    "https://api.covid19api.com/country/Jordan/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"
   );
 
   const fetchAllData = async () => {
